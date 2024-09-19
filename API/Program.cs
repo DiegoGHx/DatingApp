358b25e7da961ba5199759e21d167941dcf3c916
Diego García <diegoche@live.com.mx>
2024-09-19 08:04:32 -0600

    Parcial01 - Add Token service
    Parcial01 - Add Token service

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
