b0220c6845cb5d372aa36b20d2e71acac22981d7
Diego García <diegoche@live.com.mx>
2024-10-14 21:35:34 -0600

    Parcial02 - Add tests for errors from SPA

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -3,6 +3,7 @@ namespace API.Controllers;
 using API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 public class BuggyController(DataContext context) : BaseApiController
 {
     [Authorize]
@@ -19,7 +20,7 @@ public class BuggyController(DataContext context) : BaseApiController
             throw new ArgumentException("Server error occured!");
         return "random text";
     }
-    
+
     [HttpGet("bad-request")]
     public ActionResult<string> GetBadRequest() => BadRequest("Bad request happened");
 }
