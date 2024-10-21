af346f415643d77594e30da8962bda81d987a012
Diego García <diegoche@live.com.mx>
2024-10-20 23:01:05 -0600

    Parcial02 - Seed data

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,13 @@
-using System.Text;
 using API.Data;
 using API.Extensions;
-using API.Services;
 using API.Middlewares;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
@@ -18,4 +15,19 @@ app.UseCors((cors) => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins("http:/
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsersAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during migration/seeding.");
+}
+
 app.Run();
