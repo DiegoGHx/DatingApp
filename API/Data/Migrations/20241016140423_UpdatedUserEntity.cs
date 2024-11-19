50238c9794e6545e97fce4061ad5175e2e7ec665
Diego García <diegoche@live.com.mx>
2024-11-19 11:22:23 -0600

    Parcial03 - Update tests

--- API/Data/Migrations/20241016140423_UpdatedUserEntity.cs
+++ API/Data/Migrations/20241016140423_UpdatedUserEntity.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
+
 #nullable disable
+
 namespace API.Data.Migrations
 {
+    [ExcludeFromCodeCoverage]
     /// <inheritdoc />
     public partial class UpdatedUserEntity : Migration
     {
@@ -15,57 +19,67 @@ namespace API.Data.Migrations
                 type: "TEXT",
                 nullable: false,
                 defaultValue: new DateOnly(1, 1, 1));
+
             migrationBuilder.AddColumn<string>(
                 name: "City",
                 table: "Users",
                 type: "TEXT",
                 nullable: false,
                 defaultValue: "");
+
             migrationBuilder.AddColumn<string>(
                 name: "Country",
                 table: "Users",
                 type: "TEXT",
                 nullable: false,
                 defaultValue: "");
+
             migrationBuilder.AddColumn<DateTime>(
                 name: "Created",
                 table: "Users",
                 type: "TEXT",
                 nullable: false,
                 defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
             migrationBuilder.AddColumn<string>(
                 name: "Gender",
                 table: "Users",
                 type: "TEXT",
                 nullable: false,
                 defaultValue: "");
+
             migrationBuilder.AddColumn<string>(
                 name: "Interests",
                 table: "Users",
                 type: "TEXT",
                 nullable: true);
+
             migrationBuilder.AddColumn<string>(
                 name: "Introduction",
                 table: "Users",
                 type: "TEXT",
                 nullable: true);
+
             migrationBuilder.AddColumn<string>(
                 name: "KnownAs",
                 table: "Users",
                 type: "TEXT",
                 nullable: false,
                 defaultValue: "");
+
             migrationBuilder.AddColumn<DateTime>(
                 name: "LastActive",
                 table: "Users",
                 type: "TEXT",
                 nullable: false,
                 defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
             migrationBuilder.AddColumn<string>(
                 name: "LookingFor",
                 table: "Users",
                 type: "TEXT",
                 nullable: true);
+
             migrationBuilder.CreateTable(
                 name: "Photos",
                 columns: table => new
@@ -87,43 +101,55 @@ namespace API.Data.Migrations
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
+
             migrationBuilder.CreateIndex(
                 name: "IX_Photos_AppUserId",
                 table: "Photos",
                 column: "AppUserId");
         }
+
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "Photos");
+
             migrationBuilder.DropColumn(
                 name: "BirthDay",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "City",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "Country",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "Created",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "Gender",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "Interests",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "Introduction",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "KnownAs",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "LastActive",
                 table: "Users");
+
             migrationBuilder.DropColumn(
                 name: "LookingFor",
                 table: "Users");
