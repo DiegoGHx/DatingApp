1073152efcd0752d0ab4c93fc9bca79425a9769c
Diego García <95442993+DiegoGHx@users.noreply.github.com>
2025-02-04 22:06:38 -0600

    Parcial03 (#4)
    * Parcial03 - Add members support
    
    * Parcial03 - Add member card component
    
    * Parcial03 - Add style to member cards
    
    * Parcial03 - Add style to cards
    
    * Parcial03 - Add JWT Interceptor
    
    * Parcial03 - Add routing to member detail page
    
    * Parcial03 - Add style to member detail component
    
    * Parcial03 - Add photo gallery
    
    * Parcial03 - Execute npm audit fix
    
    * Parcial03 - Add member edit component
    
    * Parcial03 - Update member edit component
    
    * Parcial03 - Add update profile
    
    * Parcial03 - Add CanDeactivate route guard
    
    * Parcial03 - Update user repository
    
    * Parcial03 - Update member update in SPA
    
    * Parcial03 - Add loading indicator
    
    * Parcial03 - Add data recovery from app state (cache)
    
    * Parcial03 - Fix to ngx spinner type
    
    * Parcial03 - Add UNIT TESTS
    
    * Parcial03 - Update gitignore
    
    * Parcial03 - Update buggy controller tests
    
    * Parcial03 - Update tests
    
    * Parcial03 - Update tests 85% coverage
    
    * Parcial03 - Update tests 86% coverage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,26 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
   title = 'Date me';
-  
+
   ngOnInit(): void {
     this.setCurrentUser();
   }
 
-  setCurrentUser(): void{
+  setCurrentUser(): void {
     const userString = localStorage.getItem("user");
     if (!userString) return;
     const user = JSON.parse(userString);
